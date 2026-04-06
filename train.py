b3dfcc752fe8d3cdeb4852e7598cb9308d16b117
Caro <carolinacontiu@yahoo.com>
2026-04-06 21:57:01 +0300

    exp1: RandomForest n_estimators=500 tuned hyperparams val_accuracy 0.9333

--- train.py
+++ train.py
@@ -42,8 +42,10 @@ def train():
     # --- Agent modifies below this line ---
 
     model = RandomForestClassifier(
-        n_estimators=100,
+        n_estimators=500,
         max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
         random_state=42,
     )
     model.fit(X_train, y_train)
